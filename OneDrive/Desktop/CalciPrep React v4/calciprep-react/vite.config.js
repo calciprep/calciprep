d4b2d90f0d9617a2249b608e8be1768ba8c72ef1
calciprep <Calciprep@gmail.com>
2025-10-17 00:24:09 +0530

    feat: Add GitHub Actions workflow to deploy site

--- OneDrive/Desktop/CalciPrep React v4/calciprep-react/vite.config.js
+++ OneDrive/Desktop/CalciPrep React v4/calciprep-react/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Add this line
+  base: '/CalciPrep..github.io/',
   plugins: [react()],
 })
-
